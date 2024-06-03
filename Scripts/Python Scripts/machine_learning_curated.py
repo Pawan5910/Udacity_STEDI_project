19c092bb83017fb5d2a271b5ef3c17cf71139dd3
Pawan5910 <95497950+Pawan5910@users.noreply.github.com>
2024-06-03 19:10:59 +0100

    Update machine_learning_curated.py

--- Scripts/Python Scripts/machine_learning_curated.py
+++ Scripts/Python Scripts/machine_learning_curated.py
@@ -25,7 +25,7 @@ Accelerometertrusted_node1717365371234 = glueContext.create_dynamic_frame.from_c
 StepTrainerTrusted_node1717365372399 = glueContext.create_dynamic_frame.from_catalog(database="stedi", table_name="step_trainer_trusted", transformation_ctx="StepTrainerTrusted_node1717365372399")
 
 # Script generated for node SQL Query
-SqlQuery318 = '''
+SqlQuery1016 = '''
 SELECT
     act.timestamp,
     act.x,
@@ -41,9 +41,11 @@ JOIN
 ON 
     act.timestamp = stt.sensorreadingtime;
 '''
-SQLQuery_node1717365452128 = sparkSqlQuery(glueContext, query = SqlQuery318, mapping = {"act":Accelerometertrusted_node1717365371234, "stt":StepTrainerTrusted_node1717365372399}, transformation_ctx = "SQLQuery_node1717365452128")
+SQLQuery_node1717365452128 = sparkSqlQuery(glueContext, query = SqlQuery1016, mapping = {"act":Accelerometertrusted_node1717365371234, "stt":StepTrainerTrusted_node1717365372399}, transformation_ctx = "SQLQuery_node1717365452128")
 
 # Script generated for node Amazon S3
-AmazonS3_node1717365520497 = glueContext.write_dynamic_frame.from_options(frame=SQLQuery_node1717365452128, connection_type="s3", format="json", connection_options={"path": "s3://paw-lake-house/step_trainer/curated/", "partitionKeys": []}, transformation_ctx="AmazonS3_node1717365520497")
-
+AmazonS3_node1717365520497 = glueContext.getSink(path="s3://paw-lake-house/step_trainer/curated/", connection_type="s3", updateBehavior="UPDATE_IN_DATABASE", partitionKeys=[], enableUpdateCatalog=True, transformation_ctx="AmazonS3_node1717365520497")
+AmazonS3_node1717365520497.setCatalogInfo(catalogDatabase="stedi",catalogTableName="machine_learning_curated")
+AmazonS3_node1717365520497.setFormat("json")
+AmazonS3_node1717365520497.writeFrame(SQLQuery_node1717365452128)
 job.commit()
